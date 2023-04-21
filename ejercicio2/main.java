1834d179a97cfe71d2d441b2d146e82b2d8da99c
kiotoks <nikogr2005@gmail.com>
2023-04-21 09:56:11 -0300

    archivos nicolas

---
+++ ejercicio2/main.java
@@ -0,0 +1,7 @@
+package ejercicio2;
+public class main{
+    Asignacion asig = new Asignacion()
+    Direccion dir  = new Direccion("El calle", "caba", 1, 123);
+    SueldoNeto sueldo = new SueldoNeto("f","repostero", asig); 
+    Empleado repostera = new Empleado("Julian", 1234, dir, 19823, sueldo); 
+}
